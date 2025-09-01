80908dfc3c4261857637ecd8d1ee45e32571fe56
pheasa-dev <pheasayet@gmail.com>
2025-09-01 14:45:13 +0700

    test on RoomCriterailBuilder

--- src/test/java/com/pheasa/roomservice/RoomServiceApplicationTests.java
+++ src/test/java/com/pheasa/roomservice/RoomServiceApplicationTests.java
@@ -9,22 +9,19 @@ import com.pheasa.roomservice.repository.RoomCustomRepository;
 import com.pheasa.roomservice.repository.RoomRepository;
 import com.pheasa.roomservice.service.RoomService;
 import com.pheasa.roomservice.util.RoomCriteriaBuilder;
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static reactor.core.publisher.Mono.when;
 
@@ -50,21 +47,19 @@ class RoomServiceApplicationTests {
 
         Room room = new Room();
         room.setName("Luxury");
-
         //when
 //        when(roomRepository.save(room)).thenReturn(Mono.just(room));
 //        when((Publisher<?>) roomMapper.toRoom(roomDTO)).thenReturn(room);
 //        when((Publisher<?>) roomMapper.toRoomDTO(room)).thenReturn(roomDTO);
-
-
         //verify(roomMapper.toRoom(room), time(1));
         //then
 //        StepVerifier.create(roomService.createRoom(roomDTO))
 //                .expectNext(roomDTO)
 //                .verifyComplete();
     }
+
     @Test
-    void shoudReturnEmptyCrituria_whenNoFilterProvided(){
+    void shoudReturnEmptyCrituria_whenNoFilterProvided() {
         //given
         RoomFilterDTO filter = new RoomFilterDTO();
         //when
@@ -72,8 +67,9 @@ class RoomServiceApplicationTests {
         //then
         assertThat(criteria.getCriteriaObject().isEmpty());
     }
+
     @Test
-    void shouldAddNameCriterai_whenNameProvided(){
+    void shouldAddNameCriterai_whenNameProvided() {
         //given
         RoomFilterDTO filter = new RoomFilterDTO();
         filter.setName("Luxury Room");
@@ -81,6 +77,111 @@ class RoomServiceApplicationTests {
         Criteria criteria = RoomCriteriaBuilder.build(filter);
         String json = criteria.getCriteriaObject().toJson();
         //then
-        assertThat(json).contains("name","Luxury Room");
+        assertThat(json).contains("name", "Luxury Room");
     }
+
+    @Test
+    void shouldAddFloorCriterai_whenFloorProvided() {
+        //given
+        RoomFilterDTO filter = new RoomFilterDTO();
+        filter.setFloor(2);
+        //when
+        Criteria criteria = RoomCriteriaBuilder.build(filter);
+        String json = criteria.getCriteriaObject().toJson();
+        //then
+        assertThat(json).contains("floor", "2");
+    }
+
+    @Test
+    void shouldAddPriceCriterai_withOperationLT() {
+        //given
+        RoomFilterDTO filter = new RoomFilterDTO();
+        filter.setPrice(60d);
+        filter.setPriceOp("lt");
+        //when
+        Criteria criteria = RoomCriteriaBuilder.build(filter);
+        String json = criteria.getCriteriaObject().toJson();
+        //then
+        assertThat(json).contains("price").contains("$lt");
+    }
+
+    @Test
+    void shouldAddPriceCriterai_withOperationLTE() {
+        //given
+        RoomFilterDTO filter = new RoomFilterDTO();
+        filter.setPrice(60d);
+        filter.setPriceOp("lte");
+        //when
+        Criteria criteria = RoomCriteriaBuilder.build(filter);
+        String json = criteria.getCriteriaObject().toJson();
+        //then
+        assertThat(json).contains("price").contains("$lte");
+    }
+
+    @Test
+    void shouldAddPriceCriterai_withOperationGT() {
+        //given
+        RoomFilterDTO filter = new RoomFilterDTO();
+        filter.setPrice(60d);
+        filter.setPriceOp("gt");
+        //when
+        Criteria criteria = RoomCriteriaBuilder.build(filter);
+        String json = criteria.getCriteriaObject().toJson();
+        //then
+        assertThat(json).contains("price").contains("gt");
+    }
+
+    @Test
+    void shouldAddPriceCriterai_withOperationGTE() {
+        //given
+        RoomFilterDTO filter = new RoomFilterDTO();
+        filter.setPrice(60d);
+        filter.setPriceOp("gte");
+        //when
+        Criteria criteria = RoomCriteriaBuilder.build(filter);
+        String json = criteria.getCriteriaObject().toJson();
+        //then
+        assertThat(json).contains("price").contains("$gte");
+    }
+
+    @Test
+    void shouldAddPriceCriterai_withOperationEQ() {
+        //given
+        RoomFilterDTO filter = new RoomFilterDTO();
+        filter.setPrice(60d);
+        filter.setPriceOp("eq");
+        //when
+        Criteria criteria = RoomCriteriaBuilder.build(filter);
+        String json = criteria.getCriteriaObject().toJson();
+        //then
+        assertThat(json).contains("price").contains("60");
+    }
+
+    @Test
+    void shouldAddPricMin_PricMax() {
+        //given
+        RoomFilterDTO filter = new RoomFilterDTO();
+        filter.setPriceMin(60d);
+        filter.setPriceMax(80d);
+        //when
+        Criteria criteria = RoomCriteriaBuilder.build(filter);
+        String json = criteria.getCriteriaObject().toJson();
+        //then
+        assertThat(json).contains("price").contains("$gte").contains("$lte");
+    }
+
+//    @Test
+//    public void sort_withValidFieldASC() {
+//        //given
+//        RoomFilterDTO filter = new RoomFilterDTO();
+//        filter.setDirection("asc");
+//        filter.setSortBy("price");
+//        //when
+//        Sort sort = RoomCriteriaBuilder.sort(filter);
+//        //then
+//        assertThat(sort.getOrderFor("attributes.price")).isNotNull();
+//        assertThat(sort.getOrderFor("attributes.price").getDirection())
+//                .isEqualTo(Sort.Direction.ASC);
+//
+//    }
 }
